d852e6e45bc0bde76cfd06ee251a76da4585c3b5
Anhnhi Tran <anhtran811@gmail.com>
2022-10-24 21:51:07 -0600

    Fixed variable name

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,17 @@
 class Player
-  attr_reader :full_name, :first_name, :last_name, :monthly_cost, :contract_length, :nickname
+  attr_reader :player, :first_name, :last_name, :monthly_cost, :contract_length, :nickname
 
-  def initialize(full_name, monthly_cost, contract_length)
-    @full_name = full_name
-    @first_name = full_name.split[0]
-    @last_name = full_name.split[1]
+  def initialize(player, monthly_cost, contract_length)
+    @player = player
+    @first_name = player.split[0]
+    @last_name = player.split[1]
     @monthly_cost = monthly_cost
     @contract_length = contract_length
     @nickname = @nickname
   end
 
   def total_cost
-    monthly_cost * contract_length
+    @monthly_cost * @contract_length
   end
 
   def set_nickname!(nickname)
