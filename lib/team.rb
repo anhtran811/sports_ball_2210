e94e9e43c2cb9cef4ea2276b568e1ad34af2b133
Anhnhi Tran <anhtran811@gmail.com>
2022-10-24 22:35:14 -0600

    On iteration 4 but with errors

--- lib/team.rb
+++ lib/team.rb
@@ -12,4 +12,51 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    long_term_contract = []
+    @roster.each do |player|
+      if player.contract_length > 24
+        long_term_contract << player
+      end
+    end
+    long_term_contract
+  end
+
+  def short_term_players
+    short_term_contract = []
+    @roster.each do |player|
+      if player.contract_length <= 24
+        short_term_contract << player
+      end
+    end
+    short_term_contract
+  end
+
+  def total_value
+    total = []
+    @roster.each do |player|
+      total << player.total_cost
+    end
+    total.sum
+  end
+
+  def details
+    team = {
+      "total_value" => total_value,
+      "player_count" => player_count
+    }
+  end
+
+  def average_cost_of_player
+    total_value / @roster.count
+  end
+
+  def players_by_last_name
+    last_names = " "
+    @roster.each do |name|
+      last_names << name.last_name
+    end
+    last_names
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -48,4 +48,116 @@ RSpec.describe Team do
     expect(team.roster).to eq([player_1, player_2])
     expect(team.player_count).to eq(2)
   end
+
+  it 'can be a long term contract' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.long_term_players).to eq([player_1, player_3])
+  end
+
+    it 'can be short term contract' do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      expect(player_1).to be_a(Player)
+
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      expect(player_2).to be_a(Player)
+
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      expect(player_3).to be_a(Player)
+ 
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+      expect(player_4).to be_a(Player)
+ 
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.short_term_players).to eq([player_2, player_4])
+  end
+
+  it 'can have a total value' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq(85200000)
+  end
+
+  it 'can have details' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+    
+    expect(team.details).to be_a(Hash)
+    expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+  end
+
+  xit 'can have an average cost of player' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.average_cost_of_player).to eq("$21,300,000")
+  end
+
+  it 'can sort players by last name' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
+  end
 end
