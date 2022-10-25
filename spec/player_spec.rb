aa7913d75b9818897db8c91f697dddc94d63e6de
Anhnhi Tran <anhtran811@gmail.com>
2022-10-24 21:52:44 -0600

    Fixed player testing spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,16 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'can have a nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+  
+    expect(player.nickname).to eq(nil)
+  end
+
+  it 'can set a nickname' do 
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+  
+   player.set_nickname!("Squints")
+  end
 end
