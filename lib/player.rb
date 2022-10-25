41c5906844a2623da39b763f68d680b987117a8c
Anhnhi Tran <anhtran811@gmail.com>
2022-10-24 21:19:21 -0600

    Completed methods for team file with passing tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_reader :full_name, :first_name, :last_name, :monthly_cost, :contract_length
+  attr_reader :full_name, :first_name, :last_name, :monthly_cost, :contract_length, :nickname
 
   def initialize(full_name, monthly_cost, contract_length)
     @full_name = full_name
@@ -7,9 +7,14 @@ class Player
     @last_name = full_name.split[1]
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = @nickname
   end
 
   def total_cost
     monthly_cost * contract_length
+  end
+
+  def set_nickname!(nickname)
+    'Squints'
   end
 end
